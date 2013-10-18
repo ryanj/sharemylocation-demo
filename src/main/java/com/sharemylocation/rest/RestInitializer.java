cb9745ed2a903b34866f990dbf5fe41987cec64a
Shekhar Gulati <shekhargulati84@gmail.com>
2013-10-18 04:29:09 +0200

    deleted html and jsp files. deleted web.xml. Updated pom.xml to use newer version of maven war plugin. Also added configuration in pom.xml failOnMissingWebXml. Added RestInitializer and PingResource

---
+++ src/main/java/com/sharemylocation/rest/RestInitializer.java
@@ -0,0 +1,9 @@
+package com.sharemylocation.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class RestInitializer extends Application {
+
+}
